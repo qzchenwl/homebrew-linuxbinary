370728f79443d6da119ff40691d7df7b1214b365
chenwl <qzchenwl@gmail.com>
2017-04-23 16:46:46 +0800

    Update sqltabs to 0.16

--- Formula/sqltabs.rb
+++ Formula/sqltabs.rb
@@ -1,7 +1,7 @@
 class Sqltabs < Formula
     desc "SQL Tabs is an open source cross platform desktop client for postgresql."
     homepage "http://www.sqltabs.com/"
-    version "0.12"
+    version "0.16"
 
     url "https://github.com/sasha-alias/sqltabs/releases/download/v0.16.0/sqltabs.linux.tar.gz"
     sha256 "ddfbf8037f2d53783c079247935d5f373f6ab2933ea386055fc38f66ba8c9c77"
